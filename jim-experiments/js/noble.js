211609c6b914804a867cecac2ade8ff5af2a92f2
sos0 <so.c.choi@gmail.com>
2013-11-20 22:38:18 -0800

    added images and detailed story

--- jim-experiments/js/noble.js
+++ jim-experiments/js/noble.js
@@ -2,13 +2,12 @@ var timeline = {
   "timeline": {
     
       ///////// Some stuff to show when the timeline first loads /////////
-      "headline":"So Choi's Immigration Timeline",
+      "headline":"So Choi's Story",
       "type":"default",
       "text":"<p>When does it end?</p>",
       "asset": {
-          "media":"img/citizenship.jpg",
-          "credit":"Credit Name Goes Here",
-          "caption":"Caption text goes here"
+          "media":"img/1.jpg",
+          "caption":"On roof of one San Francisco condo"
       },
       
       ///////// Individual timeline entries /////////
@@ -17,86 +16,78 @@ var timeline = {
               "startDate":"2001,04,01",
               "headline":"Entry to United States",
               "text":"<p>Immigrated with both parents and younger sister on April Fool's Day.</p>",
-              "classname":"abcdef",
               "asset": {
-                  "media":"img/flag-big.jpg",
-                  "credit":"Credit Name Goes Here",
-                  "caption":"Caption text goes here",
-                  "thumbnail": "img/flag.png"
+                  "media":"img/2.jpg",
+                  "caption":"Younger sister, mother, So, father",
+                  "thumbnail": "img/2.png"
               }
           },
           
           {
-              "startDate":"2003,01,10",
+              "startDate":"2002,01,10",
               "endDate":"2010,04,01",
               "headline":"Applied for Green Card",
-              "text":"<p>Body text goes here, some HTML is OK</p>",
+              "text":"<p>Family friend offers to sponsor us for our green card application.</p>",
               "classname":"optionaluniqueclassnamecanbeaddedhere",
               "asset": {
-                  "media":"img/sunset.jpg",
-                  "credit":"Credit Name Goes Here",
-                  "caption":"Caption text goes here"
+                  "media":"img/passport.jpg",
+                  "caption":"This is a stock photo."
               }        
           },
           
           {
-              "startDate":"2010,04,01",
+              "startDate":"2011,04,01",
               "headline":"Green card application rejection",
-              "text":"<p>Body text goes here, some HTML is OK</p>",
-              "classname":"optionaluniqueclassnamecanbeaddedhere",
+              "text":"<p>Green card sponsors were not eligible to sponsor us due to their reported income level. (Their actual income level was higher, but they reported less to pay less taxes.)</p>",
+              "classname":"rejection",
               "asset": {
                   "media":"img/passport.jpg",
-                  "credit":"Credit Name Goes Here",
-                  "caption":"Caption text goes here"
+                  "caption":"This is a stock photo."
               }
           },
           
           {
-              "startDate":"2010,05,01",
-              "endDate":"2013,07,13",
-              "headline":"Appeal for Green card rejection",
-              "text":"<p>Body text goes here, some HTML is OK</p>",
-              "classname":"optionaluniqueclassnamecanbeaddedhere",
+              "startDate":"2011,05,01",
+              "endDate":"2012,07,15",
+              "headline":"Appeal for Green card",
+              "text":"<p>We file an appeal for the green card application, gathering as many documents as we can to prove that our sponsors did indeed have sufficient income.</p>",
+              "classname":"rejection",
               "asset": {
                   "media":"img/passport.jpg",
-                  "credit":"Credit Name Goes Here",
-                  "caption":"Caption text goes here"
+                  "caption":""
               }
           },
           
           {
-              "startDate":"2010,06,01",
+              "startDate":"2011,06,01",
               "headline":"Visa change to dependent of student visa holder",
-              "text":"<p>Body text goes here, some HTML is OK</p>",
+              "text":"<p>To avoid becoming out of status, my father enrolls in a college in Oakland. My sister and I automatically attain a dependent student visa F-2.</p>",
               "classname":"optionaluniqueclassnamecanbeaddedhere",
               "asset": {
                   "media":"img/passport.jpg",
-                  "credit":"Credit Name Goes Here",
-                  "caption":"Caption text goes here"
+                  "caption":""
               }
           },
           
           {
               "startDate":"2012,06,01",
               "headline":"Father's F1 expires after his graduation",
-              "text":"<p>Subsequently, he does not file for OPT and I, under a dependent visa, become out of status.</p>",
-              "classname":"optionaluniqueclassnamecanbeaddedhere",
+              "text":"<p>He does not file for OPT and subsequently both his daughters who are under a dependent visa, become out of status.</p>",
+              "classname":"rejection",
               "asset": {
                   "media":"img/passport.jpg",
-                  "credit":"Credit Name Goes Here",
-                  "caption":"Caption text goes here"
+                  "caption":""
               }
           },
           
           {
-              "startDate":"2013,07,15",
+              "startDate":"2012,07,15",
               "headline":"Green card appeal rejection",
-              "text":"<p>The timeframe for DACA eligibility is announced at July 1, 2013, rendering my sister and I ineligible for a work permit or a driver's license.</p>",
-              "classname":"optionaluniqueclassnamecanbeaddedhere",
+              "text":"<p>Our appeals process, after two more years of waiting, finally is upheld. We are given the opportunity to appeal once more, but at this point we have no money, resources, or the energy for another go at the legal process.</br>The timeframe for DACA eligibility is announced at June 15, 2012, rendering my sister and I ineligible for a work permit or a driver's license--just by two weeks.</p>",
+              "classname":"rejection",
               "asset": {
                   "media":"img/passport.jpg",
-                  "credit":"Credit Name Goes Here",
-                  "caption":"Caption text goes here"
+                  "caption":""
               }
           }           
       ]
